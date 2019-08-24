9d030c46d1fc2102bd03ab50ad80eea14d475233
Aaron Evans <aevans26@fau.edu>
2019-08-24 14:29:50 -0400

    Will module be accessible in zeit?

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ Visit http://localhost:5000`);
 // Home route
 app.get("/", async (req, res) => {
   var fse = require('fs-extra');
-  var contents = await fse.readFile("index.html", 'utf8');
-  res.send(contents);
+  //var contents = await fse.readFile("index.html", 'utf8');
+  //res.send(contents);
+  var hello = require('./hello.js');
+  res.send(hello);
   //res.send("Welcome to a basic express App.");
 });
 
