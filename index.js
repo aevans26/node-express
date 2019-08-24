f2b3793ef45a5fc0cb495cd2ecfda01ed07a856c
Aaron Evans <aevans26@fau.edu>
2019-08-24 13:59:10 -0400

    users route now queries elephantsql postgress service

--- index.js
+++ index.js
@@ -14,12 +14,15 @@ Visit http://localhost:5000`);
 });
 
 // Home route
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Welcome to a basic express App.");
 });
 
 // Mock APIs
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  let result = await callDB('SELECT id,name,location FROM users');
+  return res.json(result.rows);
+  /*
   res.json([
     { name: "William", location: "Abu Dhabi" },
     { name: "Chris", location: "Vegas" },
@@ -27,6 +30,7 @@ app.get("/users", (req, res) => {
     { name: "Jbo", location: "Umatilla" },
     { name: "Trenton", location: "WPB" }
   ]);
+  */
 });
 
 app.post("/user", (req, res) => {
@@ -34,3 +38,45 @@ app.post("/user", (req, res) => {
 
   res.send({ status: "User created", name, location });
 });
+async function callDB(query) {
+  const pg = require('pg');
+
+  let conString = "postgres://nzivzfbg:2oocpudzBzBBsyiHlmBNhFND8Ye92fJ3@isilo.db.elephantsql.com:5432/nzivzfbg" //Can be found in the Details page
+  let client = new pg.Client(conString);
+  try {
+    await client.connect();
+  } catch(ex) {
+    throw('could not connect to postgres: ', ex);
+  }
+  let result;
+  try {
+    result = await client.query(query);
+  } catch(ex) {
+    throw('error running query: ', ex);
+  }
+  client.end();
+  return result;
+}
+
+async function callDB2(query) {
+  var pg = require('pg');
+  //or native libpq bindings
+  //var pg = require('pg').native
+
+  var conString = "postgres://nzivzfbg:2oocpudzBzBBsyiHlmBNhFND8Ye92fJ3@isilo.db.elephantsql.com:5432/nzivzfbg" //Can be found in the Details page
+  var client = new pg.Client(conString);
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query(query, function(err, result) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      console.log(result.rows);
+      // >> output: 2018-08-23T14:02:57.117Z
+      client.end();
+    });
+  });
+
+}
