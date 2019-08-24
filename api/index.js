df5c5dd9b5bbc981473cfe2ed3f2e71a5213f2b3
Aaron Evans <aevans26@fau.edu>
2019-08-24 15:43:22 -0400

    tried to add a index.txt that might be readable

--- api/index.js
+++ api/index.js
@@ -18,12 +18,12 @@ app.get("/api", async (req, res) => {
   var fse = require('fs-extra');
   var path = require('path');
   
-//  var contents = await fse.readFile(path.join(__dirname, 'index.html'), 'utf8');
+  var contents = await fse.readFile(path.join(__dirname, 'index.txt'), 'utf8');
 //  res.write(contents);
   var hello = require('./hello.js');
 //  res.write(hello);
-  res.send(__dirname + " hello.js = " + hello + "<br/><br/>index.html = ");
-//  res.send(__dirname + " hello.js = " + hello + "<br/><br/>index.html = " + contents);
+//  res.send(__dirname + " hello.js = " + hello + "<br/><br/>index.html = ");
+  res.send("__dirname = " + __dirname + "<br/><br/> hello.js = " + hello + "<br/><br/>index.html = " + contents);
 //  res.end();
   //res.send("Welcome to a basic express App.");
 });
